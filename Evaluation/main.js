beb25463694edd78acbc94772e88b12eba40a40f
YechenXu <92880749+YechenXu@users.noreply.github.com>
2023-05-07 01:23:38 -0400

    basic requirements

--- Evaluation/main.js
+++ Evaluation/main.js
@@ -16,11 +16,11 @@ const View = (()=>{
     }
     
     const creatTmp = (word, guess)=>{
+        let output_word = word.join(' ');
         let template = '';
-        template += `<div>
+        template += `
         <p class="guess_time">${guess}/10</p>
-        <p class="random_letter">${word}</p>
-        </div>`;
+        <p class="random_letter">${output_word}</p>`;
         return template;
     }
     
@@ -44,7 +44,7 @@ const Model = ((api, view)=>{
 
     class State{
         constructor(){
-            this._w = '';
+            this._w = [];
             this._g = 0;  //current guess
             this._hiddeIndexes = [];
             this._origin = [];
@@ -52,6 +52,7 @@ const Model = ((api, view)=>{
 
         set generateWord(word){
             word = word[0];
+            console.log(word);
             let hideCount = Math.floor(Math.random() * word.length);
 
             const charArray = word.split('');
@@ -61,18 +62,17 @@ const Model = ((api, view)=>{
                 const randomIndex = Math.floor(Math.random() * word.length);
                 hiddenIndexes.add(randomIndex);
             }
-          
-            const originalWord = [];
+
+            this._hiddeIndexes = [];
+            this._origin = [];
             for (const index of hiddenIndexes) {
-                originalWord.push(charArray[index]);
+                this._origin.push(charArray[index]);
+                this._hiddeIndexes.push(index);
                 charArray[index] = '_';
             }
 
-            this._w = charArray.join(' ');
-
+            this._w = charArray;
             this._g = 0;  //current guess
-            this._hiddeIndexes = Array.from(hiddenIndexes);
-            this._origin = originalWord;
             let tmp = creatTmp(this._w, this._g);
             let todoContainer = document.querySelector(domSelector.container);
             console.log(this._origin);
@@ -83,15 +83,16 @@ const Model = ((api, view)=>{
             if (input.length != 0){
                 if (this._origin.includes(input)) {
                     let hidIndex = this._origin.indexOf(input);
-                    let wordIndex = this._hiddeIndexes.indexOf(hidIndex);
-                    const charArray = this._w.split('');
-                    charArray[wordIndex] = input;
-                    this._w = charArray.join(' ');
-                    this._origin.splice(hidIndex,1);
-                    this._hiddeIndexes.splice(hidIndex,1);
+                    let wordIndex = this._hiddeIndexes[hidIndex];
+                    this._w[wordIndex] = input;
+                    console.log(this._w);
+                    this._origin[hidIndex] = '_';
+                    console.log(this._origin);
+                    console.log(this._hiddeIndexes);
                 }else {
                     this._g ++;
                 }
+
                 let tmp = creatTmp(this._w, this._g);
                 let todoContainer = document.querySelector(domSelector.container);
                 render(todoContainer, tmp);
@@ -108,7 +109,6 @@ const Model = ((api, view)=>{
 
 
 // Controller => manage data & handle users' actions
-
 const Controller = ((view, model)=>{
     const { domSelector } = view;
     const { State, getData } = model;
@@ -121,18 +121,19 @@ const Controller = ((view, model)=>{
         });
     }
 
-    const update = () => {
-        const userInput = document.querySelector(domSelector.inputBox)
-        state.updateWord = userInput.value;
+    const update = (char) => {
+        state.updateWord = char;
         
         setTimeout(() => {
-            if(state._origin.length === 0) {
+            if(!state._w.includes('_')) {
                 alert(`Congrats! You found the word`);
                 getData.then((data) => {
                     state.generateWord = data;
                 });
             } else if(state._g == 10) {
-                alert("Game over! You don't have remaining guesses");
+                let correct = 0;
+                state._origin.forEach(e => {if(e != '_'){correct += 1}})
+                alert(`Game over! You have guessed ${correct} words!`);
                 getData.then((data) => {
                     state.generateWord = data;
                 });
@@ -146,7 +147,11 @@ const Controller = ((view, model)=>{
         const btn = document.querySelector(domSelector.btn);
         
         btn.addEventListener('click', init)
-        userInput.addEventListener("input", update);
+        userInput.addEventListener("keypress", (event) => { //when press enter get the input char
+            if (event.key === "Enter") {
+                update(userInput.value);
+            }
+        });
     }
     
     // wrap all function
